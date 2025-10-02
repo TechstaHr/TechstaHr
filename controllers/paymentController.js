48cc0bde9024e64ce034f506607efc40a2c921d9
Paul Oyelabi <oyelabipaul@gmail.com>
2025-10-02 20:32:08 +0100

    chore: updated flutterwave payment handler

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const flutterwaveUtils = require('../utils/flutterwave');
 const Payroll = require('../models/Payroll');
 const Bank = require('../models/Bank');
+const BillingInfo = require('../models/BillingInfo');
 
 const PAYSTACK_SECRET_KEY = process.env.PYS_SECRET_KEY;
 const PAYSTACK_BASE_URL = 'https://api.paystack.co';
@@ -67,19 +68,27 @@ const verifyPayment = async (req, res) => {
 const triggerPayment = async (req, res) => {
   try {
     const { payrollId } = req.params;
-    const existingPayroll = Payroll.findById(payrollId);
+    const existingPayroll = await Payroll.findById(payrollId);
     if (!existingPayroll) {
       return res.status(404).json({ message: "Payroll entry not found." });
     }
-    const existingBank = Bank.findById(existingPayroll.bankId);
+
+    if (existingPayroll.paymentGateway.toLowerCase().trim() !== "flutterwave") {
+      return res.status(503).json({ message: "Couldn't process payment, only flutterwave is currently supported." });
+    }
+    if (existingPayroll.paymentDue > new Date()) {
+      return res.status(403).json({ message: "Pyament not due for processing." });
+    }
+
+    const existingBank = await Bank.findById(existingPayroll.bankId);
     if (!existingBank) {
       return res.status(404).json({ message: "Bank not found." });
     }
-    const existingBillingInfo = BillingInfo.findOne({ userId: existingPayroll.userId });
+    const existingBillingInfo = await BillingInfo.findOne({ userId: existingPayroll.userId });
     if (!existingBillingInfo) {
       return res.status(404).json({ message: "No billing info found for user." });
     }
-    data = {
+    const data = {
       currency: existingBank.currency,
       paymentAmount: existingPayroll.paymentAmount,
       bank: {
@@ -89,8 +98,11 @@ const triggerPayment = async (req, res) => {
       userId: existingPayroll.userId,
       traceId: existingPayroll.traceId,
       idempotencyKey: existingPayroll.idempotencyKey,
+      reference: existingPayroll.trxReference,
     };
     const response = await flutterwaveUtils.directTransfer(data);
+    existingPayroll.paymentStatus = "initiated";
+    await existingPayroll.save();
     res.status(200).json(response);
   } catch (err) {
     res.status(500).json({
@@ -100,7 +112,38 @@ const triggerPayment = async (req, res) => {
   }
 };
 
+const flutterwaveWebhook = async (req, res) => {
+  try{
+    const event = req.body;
+
+    if (event.type === 'transfer.disburse') {
+      const eventStatus = event.data.status.toLowerCase();
+      const trxReference = event.data.reference;
+      const existingPayroll = await Payroll.findOne({trxReference: trxReference});
+      if (!existingPayroll) {
+        return res.status(500).json({ message: 'No payroll found.' });
+      }
+      if (eventStatus === "successful") {
+        existingPayroll.paymentStatus = "completed";
+        await existingPayroll.save();
+        return res.status(200).json({ message: 'completed' });
+      } else if(eventStatus === "failed") {
+        existingPayroll.paymentStatus = "failed";
+        await existingPayroll.save();
+        return res.status(200).json({ message: 'failed' });
+      }
+    }
+
+    return res.status(500).json({ message: 'unknown event type' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error handling webhook event' });
+  }
+};
+
 module.exports = {
     initializePayment,
     verifyPayment,
+    triggerPayment,
+    flutterwaveWebhook,
 };
